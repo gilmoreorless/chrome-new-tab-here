707b2594dc822d9bc92611a5382deb48327aa1b4
Gilmore Davidson <gilmoreorless@gmail.com>
2017-03-06 17:52:29 +1100

    Added a browser action to match the keyboard shortcut

--- listener.js
+++ listener.js
@@ -1,13 +1,21 @@
 function openNewTab(origTab) {
-	var opts = {active: true};
+	var opts = { active: true };
 	if (origTab) {
 		opts.index = origTab.index + 1;
+		opts.openerTabId = origTab.id;
 	}
 	chrome.tabs.create(opts);
 }
 
 chrome.commands.onCommand.addListener(function (command) {
 	if (command == 'new-tab-here') {
-		chrome.tabs.getSelected(openNewTab);
+		chrome.tabs.query({
+			active: true,
+			currentWindow: true,
+		}, function (tabs) {
+			openNewTab(tabs[0]);
+		});
 	}
 });
+
+chrome.browserAction.onClicked.addListener(openNewTab);
